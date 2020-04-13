d6fbbee362753077672be88658e6a2a7006dcc0f
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-13 16:13:08 +0300

    used layounts and route.meta in App.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div role="main">
-    <router-view></router-view>
+    <component :is="layout">
+      <router-view></router-view>
+    </component>
   </div>
 </template>
 
 <script>
+  import AuthLayout from './layouts/AuthLayout';
+  import MainLayout from './layouts/MainLayout';
+
   export default {
     name: "App",
-    components: {},
+    components: {
+      AuthLayout,
+      MainLayout
+    },
+    computed: {
+      layout() {
+        return (this.$route.meta.layout || 'auth') + '-layout';
+      }
+    }
   }
 </script>
 
