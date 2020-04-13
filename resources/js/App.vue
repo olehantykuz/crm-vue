7345f9bb6564b5758533309fa34ed1e63b03d5bb
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-13 14:28:34 +0300

    created store, router, views

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div role="main">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        components: {
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        1
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
