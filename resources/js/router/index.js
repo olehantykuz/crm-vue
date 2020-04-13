4b2d07f460896c7e2bfef06488f1810d73780e07
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-13 16:12:07 +0300

    created routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -8,7 +8,60 @@ Vue.use(Router);
 const router = new Router({
   mode: 'history',
   routes: [
-    {name: 'home', path: '/', component: Home},
+    {
+      name: 'home',
+      path: '/',
+      meta: {layout: 'main'},
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: {layout: 'auth'},
+      component: () => import('../views/Login')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      meta: {layout: 'auth'},
+      component: () => import('../views/Register')
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      meta: {layout: 'main'},
+      component: () => import('../views/Categories')
+    },
+    {
+      path: '/detail-record',
+      name: 'detail-record',
+      meta: {layout: 'main'},
+      component: () => import('../views/DetailRecord')
+    },
+    {
+      path: '/history',
+      name: 'history',
+      meta: {layout: 'main'},
+      component: () => import('../views/History')
+    },
+    {
+      path: '/planning',
+      name: 'planning',
+      meta: {layout: 'main'},
+      component: () => import('../views/Planning')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      meta: {layout: 'main'},
+      component: () => import('../views/Profile')
+    },
+    {
+      path: '/record',
+      name: 'record',
+      meta: {layout: 'main'},
+      component: () => import('../views/Record')
+    }
   ]
 });
 
