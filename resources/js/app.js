52da999e184148a6652535c896c7009ba6bb6d74
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-14 16:50:30 +0300

    created messages plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,16 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
+import './bootstrap';
 
 import router from './router';
 import store from './store';
-import './bootstrap';
 import dateFilter from './filters/date.filter';
+import messagePlugin from './utils/message.plugin';
 
 import App from './App';
 
 Vue.use(Vuelidate);
+Vue.use(messagePlugin);
 Vue.filter('date', dateFilter);
 
 const app = new Vue({
