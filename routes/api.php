1dd259200068cfcb6f438d4b7544208eed8b8cde
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-15 09:47:59 +0300

    created register route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+    'namespace' => 'Api',
+    'middleware' => 'api',
+], function () {
+    Route::post('register', 'AuthController@register');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
