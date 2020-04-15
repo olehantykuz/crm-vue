ca9e1aded5a493d1d17572acdcf60ac0f7244b3d
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-15 12:54:20 +0300

    created getCookies, login and logout methods in suthService.js

--- resources/js/api/auth.js
+++ resources/js/api/auth.js
@@ -1,9 +1,41 @@
+const baseUrl = '/api';
+
 const register = data => {
   return axios({
-    url: `/api/register`,
+    url: `${baseUrl}/register`,
     method: 'post',
     data,
   }).then(response => response);
 };
 
-export const authService = { register };
+const login = data => {
+  return axios({
+    url: `${baseUrl}/login`,
+    method: 'post',
+    data,
+  }).then(response => {
+    localStorage.setItem('auth', JSON.stringify(true));
+
+    return response;
+  });
+};
+
+const getCookies = () => {
+  return axios({
+    url: `/sanctum/csrf-cookie`,
+    method: 'get',
+  }).then(response => response);
+};
+
+const logout = () => {
+  return axios({
+    url: `${baseUrl}/logout`,
+    method: 'post',
+  }).then(response => {
+    localStorage.removeItem('auth');
+
+    return response;
+  });
+};
+
+export const authService = { register, getCookies, login, logout };
