fd3c3b99cc4495a77b9b946c7bd1b2f2cda3732f
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-15 12:47:02 +0300

    created methods login and logout in AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\RegisterRequest;
+use App\Http\Requests\Api\LoginRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 
@@ -30,5 +32,40 @@ class AuthController extends Controller
         ]);
 
         return new JsonResponse('', 201);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), with(new LoginRequest())->rules());
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $credentials = request(['email', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+
+        return response()->json([
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function logout()
+    {
+        Auth::guard('web')->logout();
+
+        return new JsonResponse(null, 204);
     }
 }
