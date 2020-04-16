e8274e5e32eb92adb418028b2e6491c8fca6afeb
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-16 08:24:51 +0300

    implemented method errorResponse in parrent controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,24 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param array|string $errors
+     * @param int|null $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function errorResponse($errors, ?int $code = 400)
+    {
+        $message = '';
+
+        if (is_array($errors)) {
+            foreach ($errors as $error) {
+               $message = is_array($error) ? $error[0] : $error;
+            }
+        } else {
+            $message = $errors;
+        }
+
+        return response()->json(['error' => $message], $code);
+    }
 }
