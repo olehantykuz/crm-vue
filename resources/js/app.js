655416761a020b27bae3c6a92db8c526eb63a68f
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-17 08:34:31 +0300

    created Loader component

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,12 +6,14 @@ import router from './router';
 import store from './store';
 import dateFilter from './filters/date.filter';
 import messagePlugin from './utils/message.plugin';
+import Loader from './components/app/Loader';
 
 import App from './App';
 
 Vue.use(Vuelidate);
 Vue.use(messagePlugin);
 Vue.filter('date', dateFilter);
+Vue.component('Loader', Loader);
 
 // eslint-disable-next-line no-new
 new Vue({
