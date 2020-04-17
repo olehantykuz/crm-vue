a3630974f5952ed935e7bc1bb6e63fe0d74ce3d5
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-17 14:53:56 +0300

    created CurrencyController

---
+++ app/Http/Controllers/Api/CurrencyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\Currency as CurrencyResource;
+use App\Services\CurrencyService;
+
+class CurrencyController extends Controller
+{
+    /**
+     * @param CurrencyService $service
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function conversation(CurrencyService $service)
+    {
+        return response()->json([
+            'currencies' => CurrencyResource::collection($service->getAll()),
+            'baseCurrency' => $service->getDefaultCurrencyCode(),
+        ]);
+    }
+}
