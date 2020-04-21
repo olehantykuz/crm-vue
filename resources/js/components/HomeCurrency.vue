6ac0b3852591671baa60a8de5c2a6dcbac840ac5
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-21 14:09:34 +0300

    aded ability select currency for displaying conversation rate

--- resources/js/components/HomeCurrency.vue
+++ resources/js/components/HomeCurrency.vue
@@ -3,7 +3,16 @@
     <div class="card orange darken-3 bill-card">
       <div class="card-content white-text">
         <div class="card-header">
-          <span class="card-title">Курс валют {{baseCurrency}}</span>
+          <span class="card-title col s12 m10">Курс валют {{selected}}</span>
+          <select v-model="selected" class="col s12 m2">
+            <option
+              v-for="curr of currencyCodes"
+              :key="curr"
+              :value="curr"
+            >
+              {{curr}}
+            </option>
+          </select>
         </div>
         <table>
           <thead>
@@ -17,7 +26,7 @@
           <tbody>
           <tr v-for="curr of currencyCodes" :key="curr">
             <td>{{curr}}</td>
-            <td>{{ rates[curr].rate.toFixed(6) }}</td>
+            <td>{{ getConversation(curr) }}</td>
             <td>{{ (rates[curr].date * 1000) | date('date') }}</td>
           </tr>
           </tbody>
@@ -31,9 +40,24 @@
 export default {
   name: 'HomeCurrency',
   props: ['rates', 'baseCurrency', 'currencyCodes'],
+  data: () => ({
+    selected: '',
+  }),
+  methods: {
+    getConversation(currentCode) {
+      return this.selected
+        ? (this.rates[currentCode].rate / this.rates[this.selected].rate).toFixed(6)
+        : null;
+    },
+  },
+  mounted() {
+    this.selected = this.baseCurrency;
+  },
 };
 </script>
 
 <style scoped>
-
+  select {
+    display: block;
+  }
 </style>
