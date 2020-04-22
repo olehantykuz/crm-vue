b03a0e1cbfa6fa125a6ebc153071679633809352
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-22 10:49:55 +0300

    updated create user in user service: added creation default options

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Models\Currency;
+use App\Models\DefaultOptions;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,10 +15,19 @@ class UserService
      */
     public function create(array $data)
     {
-        return User::create([
+        $currency = Currency::find($data['currencyId']);
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $defaultOptions = new DefaultOptions();
+        $defaultOptions->monthly_budget = $data['monthlyBudget'];
+        $defaultOptions->currency()->associate($currency);
+        $defaultOptions->user()->associate($user);
+        $defaultOptions->save();
+
+        return $user;
     }
 }
