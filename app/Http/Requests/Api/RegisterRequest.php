f5669e295053c1f62b722936a31a6dd32e266c6e
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-22 10:39:59 +0300

    updated validation rules in RegisterRequest

--- app/Http/Requests/Api/RegisterRequest.php
+++ app/Http/Requests/Api/RegisterRequest.php
@@ -27,6 +27,8 @@ class RegisterRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6'],
+            'currencyId' => ['required', 'integer', 'exists:currencies,id'],
+            'monthlyBudget' => ['required', 'integer', 'min:1'],
         ];
     }
 }
