27335e9e702a19f3a7f978147ce3c95dd3bf74cc
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-22 16:54:09 +0300

    added endpoint for creation category

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,10 @@ Route::group([
     Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::post('logout', 'AuthController@logout');
         Route::get('me', 'AuthController@user');
+
+        Route::group(['prefix' => 'categories'], function () {
+            Route::post('/', 'CategoryController@store');
+        });
     });
 
 });
