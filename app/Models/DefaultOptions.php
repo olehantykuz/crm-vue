54ce2b43fdb06901a80ac0d5cc981902ce2df0df
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-23 08:18:36 +0300

    added soft deletes to exists models

--- app/Models/DefaultOptions.php
+++ app/Models/DefaultOptions.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DefaultOptions extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'default_options';
     protected $fillable = [
         'monthlyBudget'
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use Notifiable,
-        HasApiTokens;
+        HasApiTokens,
+        SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
