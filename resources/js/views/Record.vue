ce9f7c2782d6a0f09aac43ca2b2c76e2452ca8a1
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-27 13:44:16 +0300

    added validation on record page and collect form data

--- resources/js/views/Record.vue
+++ resources/js/views/Record.vue
@@ -10,7 +10,7 @@
     >
       Категорий пока нет. <router-link to="/categories">Добавить новую категорию</router-link>
     </p>
-    <form class="form" v-else>
+    <form class="form" v-else @submit.prevent="submitHandler">
       <select-category
         v-if="categories.length"
         :categories="categories"
@@ -42,23 +42,48 @@
         </label>
       </p>
 
-      <div class="input-field">
-        <input
-          id="amount"
-          type="number"
-        >
-        <label for="amount">Сумма</label>
-        <span class="helper-text invalid">amount пароль</span>
+      <div class="row">
+        <div class="input-field  col s8 amount">
+          <input
+            id="amount"
+            type="number"
+            step="0.01"
+            v-model.number="amount"
+            :class="{invalid: $v.amount.$dirty && !$v.amount.positive}"
+          >
+          <label for="amount">Сумма</label>
+          <span
+            v-if="$v.amount.$dirty && !$v.amount.positive"
+            class="helper-text invalid"
+          >
+            Значение суммы должно быть больше 0
+          </span>
+        </div>
+        <div class="input-field col s4 amount_currency">
+          <input
+            id="category_currency"
+            type="text"
+            :disabled="true"
+            :value="currency"
+          >
+          <label for="category_currency">Валюта</label>
+        </div>
       </div>
 
       <div class="input-field">
         <input
           id="description"
           type="text"
+          v-model="description"
+          :class="{invalid: $v.description.$dirty && !$v.description.required}"
         >
         <label for="description">Описание</label>
         <span
-          class="helper-text invalid">description пароль</span>
+          v-if="$v.description.$dirty && !$v.description.required"
+          class="helper-text invalid"
+        >
+          Введите описание
+        </span>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -71,6 +96,7 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import { required } from 'vuelidate/lib/validators';
 import SelectCategory from '../components/app/SelectCategory';
 
 export default {
@@ -78,26 +104,60 @@ export default {
   data: () => ({
     loading: true,
     type: 'outcome',
+    amount: 1,
+    description: '',
   }),
+  validations: {
+    description: { required },
+    amount: { positive: (v) => v > 0 },
+  },
   components: { SelectCategory },
   computed: {
-    ...mapGetters(['categories', 'currentCategory']),
+    ...mapGetters(['currencyConversation', 'error', 'categories', 'currentCategory']),
     categoryId() {
       return this.currentCategory;
+    },
+    currency() {
+      const index = this.categories.findIndex((cat) => (cat.id === this.categoryId));
+
+      return index > -1 ? this.categories[index].currency : null;
     },
   },
   methods: {
     ...mapActions(['fetchCategories']),
+    submitHandler() {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
+      }
+      const formData = {
+        categoryId: this.categoryId,
+        amount: this.amount,
+        description: this.description,
+        type: this.type,
+        currencyId: this.currencyConversation[this.currency].id,
+      };
+
+      console.log(formData);
+    },
   },
   async mounted() {
     if (!this.categories.length) {
       await this.fetchCategories();
     }
     this.loading = false;
+    setTimeout(() => {
+      window.M.updateTextFields();
+    }, 0);
   },
 };
 </script>
 
 <style scoped>
-
+  .amount {
+    padding-left: 0;
+  }
+  .amount_currency {
+    padding-right: 0;
+  }
 </style>
