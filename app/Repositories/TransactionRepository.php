9e46bea8249af854d6aaee56bcd63a9cb4ab4f14
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-28 16:41:04 +0300

    added methods createDenormalizedRecord, updateDenormalizedRecord, getDenormalizedRecordByTransactionId to transaction repository

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
@@ -34,5 +35,47 @@ class TransactionRepository
             ->where('transactions.user_id', $userId)
             ->groupBy(['code', 'category_id', 'type'])
             ->get();
+    }
+
+    /**
+     * @param int $transactionId
+     * @param array $data
+     * @return bool
+     */
+    public function createDenormalizedRecord(int $transactionId, array $data)
+    {
+        return DB::table('dnmz_transactions')
+            ->insert([
+                'transaction_id' => $transactionId,
+                'currencies_amount' => json_encode($data),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+    }
+
+    /**
+     * @param int $transactionId
+     * @param array $data
+     * @return int
+     */
+    public function updateDenormalizedRecord(int $transactionId, array $data)
+    {
+        return DB::table('dnmz_transactions')
+            ->where('transaction_id', $transactionId)
+            ->update([
+                'currencies_amount' => json_encode($data),
+                'updated_at' => Carbon::now(),
+            ]);
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Query\Builder|object|null
+     */
+    public function getDenormalizedRecordByTransactionId(int $id)
+    {
+        return DB::table('dnmz_transactions')
+            ->where('transaction_id', $id)
+            ->first();
     }
 }
