e46883c396e545492a87c73deea4c0d4453c8cc5
Oleh Antykuz <ovantykuz@gmail.com>
2020-05-05 09:12:59 +0300

    implemented getList method in transactionDervice.js; refactored transactionService.js

--- resources/js/api/transaction.js
+++ resources/js/api/transaction.js
@@ -1,20 +1,43 @@
-const baseUrl = '/api/categories';
+const categoriesBaseUrl = '/api/categories';
+const transactionsBaseUrl = '/api/transactions';
 const { axios } = window;
 
+const buildMonthYearQueryString = (month, year) => {
+  const params = { month, year };
+
+  return Object.keys(params).map((key) => (params[key] && (`${key}=${params[key]}`))).join('&');
+};
+
 const create = (categoryId, data) => axios({
-  url: `${baseUrl}/${categoryId}/transaction`,
+  url: `${categoriesBaseUrl}/${categoryId}/transaction`,
   method: 'post',
   data,
 }).then((response) => response);
 
-const getTotals = (month, year) => axios({
-  url: `/api/transactions/amounts?month=${month}&year=${year}`,
-  method: 'get',
-}).then((response) => response);
+const getTotals = (month, year) => {
+  const queryString = buildMonthYearQueryString(month, year);
+  const url = `${transactionsBaseUrl}/amounts?${queryString}`;
+
+  return axios({
+    url,
+    method: 'get',
+  }).then((response) => response);
+};
+
+const getList = (month, year) => {
+  const queryString = buildMonthYearQueryString(month, year);
+  const url = `${transactionsBaseUrl}?${queryString}`;
+
+  return axios({
+    url,
+    method: 'get',
+  }).then((response) => response);
+};
 
 const transactionService = {
   create,
   getTotals,
+  getList,
 };
 
 export default transactionService;
