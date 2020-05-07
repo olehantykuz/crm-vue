3adbe41b272b16b494507798dad7feabb7bef947
Oleh Antykuz <ovantykuz@gmail.com>
2020-05-07 16:27:45 +0300

    extracted history table to separate component

--- resources/js/views/History.vue
+++ resources/js/views/History.vue
@@ -9,42 +9,17 @@
     </div>
 
     <section>
-      <table>
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>Сумма</th>
-          <th>Дата</th>
-          <th>Категория</th>
-          <th>Тип</th>
-          <th>Открыть</th>
-        </tr>
-        </thead>
-
-        <tbody>
-        <tr>
-          <td>1</td>
-          <td>1212</td>
-          <td>12.12.32</td>
-          <td>name</td>
-          <td>
-            <span class="white-text badge red">Расход</span>
-          </td>
-          <td>
-            <button class="btn-small btn">
-              <i class="material-icons">open_in_new</i>
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+      <history-table></history-table>
     </section>
   </div>
 </template>
 
 <script>
+import HistoryTable from '../components/HistoryTable';
+
 export default {
   name: 'History',
+  components: { HistoryTable },
 };
 </script>
 
